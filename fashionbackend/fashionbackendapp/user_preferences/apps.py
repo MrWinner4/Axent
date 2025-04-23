d63ab628b0125c458428ed73ca24124ab255ffcb
William Judd Ebert <juddebert@yahoo.com>
2025-04-22 22:30:24 -0400

    user_preferences.apps fixes

--- fashionbackend/fashionbackendapp/user_preferences/apps.py
+++ fashionbackend/fashionbackendapp/user_preferences/apps.py
@@ -5,11 +5,9 @@ class UserPreferencesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'user_preferences'
 
-def ready(self):
-    import user_preferences.signals  
-
-# In views.py or apps.py
-from .firebase import initialize_firebase
-
-# Call initialize_firebase() at the start of your app or view
-initialize_firebase()
+    def ready(self):
+        # Import signals
+        from .signals import create_or_update_user_profile
+        # Initialize Firebase
+        from .firebase import initialize_firebase
+        initialize_firebase()
