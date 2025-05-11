53b138dbaa2db7f9b85801e89604be610139e0ce
Judd Ebert <juddebert@yahoo.com>
2025-05-11 18:28:51 -0400

    jsonschema2

--- fashionbackend/fashionbackendapp/product_recommender/models.py
+++ fashionbackend/fashionbackendapp/product_recommender/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 import uuid
 from django.contrib.postgres.fields import ArrayField
-import jsonschema
 from jsonschema import validate, ValidationError
 
 
