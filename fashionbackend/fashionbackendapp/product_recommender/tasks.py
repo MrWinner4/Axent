a160c1eb6583460b7925332244e9957cc07777f1
William Judd Ebert <juddebert@yahoo.com>
2025-05-31 17:44:30 -0400

    debug

--- fashionbackend/fashionbackendapp/product_recommender/tasks.py
+++ fashionbackend/fashionbackendapp/product_recommender/tasks.py
@@ -1,4 +1,6 @@
 from django.core.management import call_command
+print("tasks.py loaded")
 
 def run_train_recommender():
+    print("run train recommender called")
     call_command('train_recommender')
