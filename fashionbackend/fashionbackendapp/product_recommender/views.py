19cbbe05d5e506cfb470fc88dc8d71bc4d1793fe
Judd Ebert <juddebert@yahoo.com>
2025-06-11 16:14:10 -0400

    more debugging

--- fashionbackend/fashionbackendapp/product_recommender/views.py
+++ fashionbackend/fashionbackendapp/product_recommender/views.py
@@ -14,11 +14,7 @@ from firebase_admin import auth as firebase_auth
 def get_user_from_token(token):
     try:
         decoded_token = firebase_auth.verify_id_token(token)
-        print("decoded_token")
-        print(decoded_token)
         firebase_uid = decoded_token['uid']
-        print("firebase_uid")
-        print(firebase_uid)
         return UserProfile.objects.get(firebase_uid=firebase_uid)
     except Exception as e:
         print(f"Error getting user from token: {e}")
@@ -47,7 +43,6 @@ class ProductViewSet(viewsets.ViewSet):
 
     @action(detail=False, methods=['get'])
     def recommend(self, request):
-        print("recommend")
         """Get product recommendations for a user"""
         auth_header = request.headers.get('Authorization', '')
         if not auth_header.startswith('Bearer '):
@@ -55,17 +50,15 @@ class ProductViewSet(viewsets.ViewSet):
 
         token = auth_header.split(' ').pop()
         user_profile = get_user_from_token(token)
-        print("user profile?")
-        print(user_profile)
         if not user_profile:
             return Response({"error": "Invalid or expired token"}, status=401)
 
         try:
-            print("filters")
-            print(request.query_params.get('filters'))
             filters = request.query_params.get('filters', '')
         except KeyError:
             return Response({"error": "Filters not provided"}, status=400)
+        
+        print(user_profile.firebase_uid)
 
         try:
             recommendations = client.send(RecommendItemsToUser(user_profile.firebase_uid, 10, filter=filters))
