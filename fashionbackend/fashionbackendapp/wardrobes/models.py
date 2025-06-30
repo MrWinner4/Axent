1a484e21312824601277928ac3434ce1acf4d702
Judd Ebert <juddebert@yahoo.com>
2025-06-30 19:05:13 -0400

    Migrations.

--- fashionbackend/fashionbackendapp/wardrobes/models.py
+++ fashionbackend/fashionbackendapp/wardrobes/models.py
@@ -9,6 +9,7 @@ class Wardrobe(models.Model):
     name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    product_ids = models.JSONField(default=list)
 
     class Meta:
         unique_together = ('user', 'name')
